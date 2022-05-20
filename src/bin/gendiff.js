3f15d82f7b1fffc3afb2a6d39d0112d5773988c5
andrey.sh <aash7133@gmail.com>
2022-05-20 17:30:02 +0700

    step 1 completed - gendiff -h -v done

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,2 +1,14 @@
 #!/usr/bin/env node
-console.log('Hello Andrey');
+import { program } from 'commander';
+// import gendiff from './src/index.js';
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.1.0', '-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information')
+  // .arguments('<filepath1> <filepath2>')
+  // .action((filepath1, filepath2, options) => {
+  // console.log(gendiff(filepath1, filepath2, options.format));
+  // });
+
+program.parse(process.argv);
