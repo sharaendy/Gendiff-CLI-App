7344d59cee49dc5bb90a3325a1bc70c2f79f98b6
andrey.sh <aash7133@gmail.com>
2022-05-23 14:43:47 +0700

    option and arguments was added

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -6,9 +6,10 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0', '-V, --version', 'output the version number')
   .helpOption('-h, --help', 'output usage information')
-  // .arguments('<filepath1> <filepath2>')
-  // .action((filepath1, filepath2, options) => {
-  // console.log(gendiff(filepath1, filepath2, options.format));
-  // });
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>');
+// .action((filepath1, filepath2, options) => {
+// console.log(gendiff(filepath1, filepath2, options.format));
+// });
 
 program.parse(process.argv);
