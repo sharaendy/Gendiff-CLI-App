bf7f1f508bbefe13a5eb9c7c92a1e07509027c37
andrey.sh <aash7133@gmail.com>
2022-06-01 17:19:07 +0700

    bin (gendiff.js) file is complete

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,22 +1,15 @@
 #!/usr/bin/env node
 import { program } from 'commander';
-// import gendiff from './src/index.js';
+import gendiff from './src/index.js';
+
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0', '-V | --version', 'output the version number')
   .helpOption('-h | --help', 'output usage information')
   .option('-f | --format <type>', 'output format')
-  // .argument('<filepath1>', 'insert first file-path')
-  // .argument('<filepath2>', 'insert second file-path')
   .arguments('<filepath1> <filepath2>')
-  // .action((filepath1, filepath2, options) => {
-  //   console.log(gendiff(filepath1, filepath2, options.format));
-  // });
+  .action((filepath1, filepath2, options) => {
+    console.log(gendiff(filepath1, filepath2, options.format));
+  });
 
 program.parse(process.argv);
-
-// const options = program.opts();
-// console.log(options.format)
-
-// if (options.format === undefined) console.log('no format');
-// if (options.format !== undefined) console.log('format done');
